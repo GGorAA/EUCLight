32bb8ca2ef024f1bceaa80edf6f2fec9de36f938
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-07 00:47:18 +0300

    update

---
+++ euclight/debug.ino
@@ -0,0 +1,4 @@
+void setupSerial() {
+  Serial.begin(9600);
+  Serial.println("Debug mode is enabled. See here some messages to debug the behaviour of EUCLight.");
+}
---
+++ euclight/euclight.ino
@@ -0,0 +1,12 @@
+#define RUN_MODE "DEBUG"
+
+void setup() {
+  if (RUN_MODE == "DEBUG") {
+    setupSerial();
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
