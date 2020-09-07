0d804cb446fdc78c6885470a7d0947d3c718e40c
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-07 16:12:24 +0300

    add settings

--- euclight/euclight.ino
+++ euclight/euclight.ino
@@ -1,6 +1,3 @@
-#define DEBUG_ENABLED true // запуск режима отладки. 
-#define LIGHTS_PIN 1 // пин, к которому подключена лента
-
 void setup() {
   if (DEBUG_ENABLED == true) {
     setupSerial();
@@ -8,5 +5,4 @@ void setup() {
 }
 
 void loop() {
-  
 }
---
+++ euclight/settings.ino
@@ -0,0 +1,2 @@
+#define DEBUG_ENABLED true // запуск режима отладки. 
+#define LIGHTS_PIN 1 // пин, к которому подключена лента
