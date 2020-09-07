64c87c4b80b274a6a7781b688a4678d67f593772
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-07 22:24:56 +0300

    renamed  some variables, added EucInterface library

--- euclight/euclight.ino
+++ euclight/euclight.ino
@@ -1,9 +1,10 @@
 #define DEBUG_ENABLED true // запуск режима отладки. 
-#define LIGHTS_PIN 1 // пин, к которому подключена лента
-
+#define LIGHTS_PIN 6 // пин, к которому подключена лента
 #define LED_COUNT 144 // Количество светодиодов во всей ленте
+
 #include "FastLED.h"
-#define PIN 6
+#include <EucInterface.h>
+
 CRGB leds[LED_COUNT];
 byte counter;
 
@@ -11,13 +12,13 @@ void setup() {
   if (DEBUG_ENABLED == true) {
     setupSerial();
   }
-  FastLED.addLeds<WS2811, PIN, GRB>(leds, LED_COUNT).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<WS2811, LIGHTS_PIN, GRB>(leds, LED_COUNT).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(50);
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-  for (int i = 0; i < NUM_LEDS; i++ ) {         // от 0 до первой трети
+  for (int i = 0; i < LED_COUNT; i++ ) {         // от 0 до первой трети
     leds[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
     // умножение i уменьшает шаг радуги
   }
