403bc9d2aa1e17369f23d015cab0054896e9b19f
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-09 01:02:12 +0300

    t

--- euclight/euclight.ino
+++ euclight/euclight.ino
@@ -11,9 +11,7 @@ byte counter;
 void setup() {
   FastLED.addLeds<WS2811, LIGHTS_PIN, GRB>(leds, LED_COUNT).setCorrection( TypicalLEDStrip ); // настройка ленты
   FastLED.setBrightness(50);
-  pinMode(13, OUTPUT);
-
-  
+  pinMode(13, OUTPUT);  
 }
 
 void loop() {
