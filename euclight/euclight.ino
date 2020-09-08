7701957857a096d1a258a296f3206ce671ce0750
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-08 14:17:41 +0300

    added FastLED lib folder

--- euclight/euclight.ino
+++ euclight/euclight.ino
@@ -3,7 +3,7 @@
 #define LED_COUNT 144 // Количество светодиодов во всей ленте
 #define WHEEL_DIAMETER 14 // Диаметр колеса. В дюймах
 
-#include "FastLED.h"
+#include "libraries/FastLED/FastLED.h"
 
 unsigned long lightStripDelayLastCalled;
 
@@ -20,14 +20,14 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < LED_COUNT; i++ ) {         // от 0 до первой трети
-    leds[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
-    // умножение i уменьшает шаг радуги
+  for (int i = 0; i < LED_COUNT; i++ ) {                      // от 0 до первой трети
+    leds[i] = CHSV(counter + i * 2, 255, 255);                // HSV. Увеличивать HUE (цвет)
+                                                              // умножение i уменьшает шаг радуги
   }
-  counter++;        // counter меняется от 0 до 255 (тип данных byte)
+  counter++;                                                  // counter меняется от 0 до 255 (тип данных byte)
   FastLED.show();
   if (millis() - lightStripDelayLastCalled >= 50 ) {
     lightStripDelayLastCalled = millis();
   }
-  delay(lightStripSpeed());         // скорость движения радуги
+  delay(lightStripSpeed());                                   // скорость движения радуги
 }
