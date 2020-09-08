1eda7b01543073708e0ba41b291aabc049eb00fa
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-09 00:57:58 +0300

    add some comments

--- euclight/euclight.ino
+++ euclight/euclight.ino
@@ -1,15 +1,15 @@
-#define LIGHTS_PIN 6 // пин, к которому подключена лента
-#define LED_COUNT 144 // Количество светодиодов во всей ленте
+#define LIGHTS_PIN 6                                            // пин, к которому подключена лента
+#define LED_COUNT 144                                           // Количество светодиодов во всей ленте
 
 #include "FastLED.h"
 
-unsigned long lightStripDelayLastCalled;
+unsigned long lightStripDelayLastCalled;                        // переменная для замены delay() при помощи millis()
 
 CRGB leds[LED_COUNT];
 byte counter;
 
 void setup() {
-  FastLED.addLeds<WS2811, LIGHTS_PIN, GRB>(leds, LED_COUNT).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<WS2811, LIGHTS_PIN, GRB>(leds, LED_COUNT).setCorrection( TypicalLEDStrip ); // настройка ленты
   FastLED.setBrightness(50);
   pinMode(13, OUTPUT);
 }
@@ -17,10 +17,10 @@ void setup() {
 void loop() {
   // Тут мы собираем данные с колеса
   int lightStripSpeed = eucLightStripSpeed()
-
+  
   
   if (millis() - lightStripDelayLastCalled >= lightStripSpeed() ) {
-    lightStripDelayLastCalled = millis();
+    lightStripDelayLastCalled = millis();                       // заново считать время
 
     for (int i = 0; i < LED_COUNT; i++ ) {                      // от 0 до первой трети
       leds[i] = CHSV(counter + i * 2, 255, 255);                // HSV. Увеличивать HUE (цвет)
