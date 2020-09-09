54dd42d603a415f8e83d24be5ac803a9e737b41c
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-09 14:21:33 +0300

    update functions

--- euclight/euclight.ino
+++ euclight/euclight.ino
@@ -1,12 +1,12 @@
 #define LIGHTS_PIN 6                                            // пин, к которому подключена лента
 #define LED_COUNT 144                                           // Количество светодиодов во всей ленте
 
-#include "FastLED.h"
+#include "FastLED.h"                                            // Библиотека для светодиодной ленты
 
 unsigned long lightStripDelayLastCalled;                        // переменная для замены delay() при помощи millis()
 
 CRGB leds[LED_COUNT];
-byte counter;
+byte lightStripCounter;
 
 void setup() {
   FastLED.addLeds<WS2811, LIGHTS_PIN, GRB>(leds, LED_COUNT).setCorrection( TypicalLEDStrip ); // настройка ленты
@@ -16,20 +16,24 @@ void setup() {
 
 void loop() {
   // Тут мы собираем данные с колеса
-  int lightStripSpeed = eucLightStripSpeed();
+  int lightStripSpeed = eucLightStripSpeed();                   // Берем данные про скорость подсветки
   
-  if (millis() - lightStripDelayLastCalled >= lightStripSpeed() ) {
-    lightStripDelayLastCalled = millis();                       // заново считать время
+  if (millis() - lightStripDelayLastCalled >= lightStripSpeed() ) { // Задержка
+    lightStripDelayLastCalled = millis();                       // Заново считать время
 
-    for (int i = 0; i < LED_COUNT; i++ ) {                      // от 0 до первой трети
-      leds[i] = CHSV(counter + i * 2, 255, 255);                // HSV. Увеличивать HUE (цвет)
-      // умножение i уменьшает шаг радуги
-    }
-    counter++;                                                  // counter меняется от 0 до 255 (тип данных byte)
-    FastLED.show();
+
   }
 }
 
-void lightStripStopSignal() {
+void lightSpeedNormal() {
+  for (int i = 0; i < LED_COUNT; i++ ) {                        // от 0 до первой трети
+      leds[i] = CHSV(lightStripCounter + i * 2, 255, 255);      // HSV. Увеличивать HUE (цвет)
+      // умножение i уменьшает шаг радуги
+    }
+    lightStripCounter++;                                        // lightStripCounter меняется от 0 до 255 (тип данных byte)
+    FastLED.show();
+}
+
+void lightStripWithStopSignal() {
 
 }
