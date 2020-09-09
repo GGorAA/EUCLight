037d5bc6d2be033edd9887f9ffdd292125aad28a
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-09 20:42:48 +0300

    deleted light strip effects file, moved som functions in main file

--- euclight/euclight.ino
+++ euclight/euclight.ino
@@ -9,6 +9,32 @@ unsigned long lightStripDelayLastCalled;                        // переме�
 CRGB lightStripLEDs[LED_COUNT];
 byte lightStripCounter;
 
+// ---------------СЛУЖЕБНЫЕ ПЕРЕМЕННЫЕ-----------------
+int BOTTOM_INDEX = 0;        // светодиод начала отсчёта
+int TOP_INDEX = int(LED_COUNT / 2);
+int EVENODD = LED_COUNT % 2;
+struct CRGB leds[LED_COUNT];
+int ledsX[LED_COUNT][3];     //-ARRAY FOR COPYING WHATS IN THE LED STRIP CURRENTLY (FOR CELL-AUTOMATA, MARCH, ETC)
+
+int thisdelay = 20;          //-FX LOOPS DELAY VAR
+int thisstep = 10;           //-FX LOOPS DELAY VAR
+int thishue = 0;             //-FX LOOPS DELAY VAR
+int thissat = 255;           //-FX LOOPS DELAY VAR
+
+int thisindex = 0;
+int thisRED = 0;
+int thisGRN = 0;
+int thisBLU = 0;
+
+int idex = 0;                //-LED INDEX (0 to LED_COUNT-1
+int ihue = 0;                //-HUE (0-255)
+int ibright = 0;             //-BRIGHTNESS (0-255)
+int isat = 0;                //-SATURATION (0-255)
+int bouncedirection = 0;     //-SWITCH FOR COLOR BOUNCE (0-1)
+float tcount = 0.0;          //-INC VAR FOR SIN LOOPS
+int lcount = 0;              //-ANOTHER COUNTING VAR
+// ---------------СЛУЖЕБНЫЕ ПЕРЕМЕННЫЕ-----------------
+
 void setup() {
   driverRunOnStartup();                                          // Стартовые функции драйвера
   FastLED.addLeds<WS2811, LIGHTS_PIN, GRB>(lightStripLEDs, LED_COUNT).setCorrection( TypicalLEDStrip ); // настройка ленты
@@ -32,7 +58,7 @@ void loop() {
     } else if (eucDeviceState() == 'back') {
 
     } else {
-      lightStripNormal()
+      lightStripNormal();
     }
   }
 }
@@ -44,4 +70,31 @@ void lightStripNormal() {
     }
     lightStripCounter++;                                        // lightStripCounter меняется от 0 до 255 (тип данных byte)
     FastLED.show();
+}
+
+
+void lightStripModesRainbowFade(int speed) {
+  ihue++;
+  if (ihue > 255) {
+    ihue = 0;
+  }
+  for (int idex = 0 ; idex < LED_COUNT; idex++ ) {
+    leds[idex] = CHSV(ihue, thissat, 255);
+  }
+  LEDS.show();
+  delay(speed);
+}
+
+void lightStripModesRainbowLoop(int speed) {
+  idex++;
+  ihue = ihue + thisstep;
+  if (idex >= LED_COUNT) {
+    idex = 0;
+  }
+  if (ihue > 255) {
+    ihue = 0;
+  }
+  leds[idex] = CHSV(ihue, thissat, 255);
+  LEDS.show();
+  delay(speed);
 }
