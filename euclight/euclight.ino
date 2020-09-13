492e077c95c4a83edb54b94b7d3bf0b8b3381ddc
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-14 01:21:51 +0300

    move of brakelight to address led strip

--- euclight/euclight.ino
+++ euclight/euclight.ino
@@ -5,20 +5,20 @@
 // Настройки главной светодиодной ленты
 constexpr uint8_t LIGHT_STRIP_PIN = 6;                          // Пин, к которому подключена главная лента
 constexpr uint8_t LIGHT_STRIP_LED_COUNT = 144;                  // Количество светодиодов во всей главной ленте
-constexpr uint8_t LIGHT_STRIP_BRIGHTNESS_ON = 50;               // Яркость ленты. Диапазон: 0 - 255
+constexpr uint8_t LIGHT_STRIP_BRIGHTNESS = 50;                  // Яркость ленты. Диапазон: 0 - 255
 #define ORDER_GRB                                               // Порядок цветов
 
 // Настройки стоп-сигнала
 // Пины
-constexpr uint8_t BRAKELIGHT_RED_PIN = 9;                       // Пин для подключения красного цвета стоп-сигнала
-constexpr uint8_t BRAKELIGHT_GREEN_PIN = 10;                    // Пин для подключения зеленого цвета стоп-сигнала
-constexpr uint8_t BRAKELIGHT_BLUE_PIN = 11;                     // Пин для подключения синего цвета стоп-сигнала
+constexpr uint8_t BRAKELIGHT_PIN = 5;                           // Пин подключения адресной ленты для стоп-сигнала
 // Свет
 constexpr uint8_t BRAKELIGHT_BRIGHTNESS_ON = 100;               // Яркость включенного стоп-сигнала. Диапазон: 0 - 255
-constexpr uint8_t BRAKELIGHT_BRIGTNESS_IDLE = 20;
+constexpr uint8_t BRAKELIGHT_BRIGTNESS_IDLE = 20;               // Яркость бездейвствующего стоп-сигнала. Диапазон: 0 - 255
 // Анимации
-//constexpr uint8_t BRAKELIGHT_ANIMATION_SPEED_ON = 0;            // Скорость анимации включения стоп-сигнала. Измеряеться в миллисекундах
-//constexpr uint8_t BRAKELIGHT_ANIMATION_SPEED_IDLE = 500;        // Скорость анимации выключения стоп-сигнала. Измеряеться в миллисекундах
+constexpr uint8_t BRAKELIGHT_ANIMATION_SPEED_ON = 0;            // Скорость анимации включения стоп-сигнала. Измеряеться в миллисекундах
+constexpr uint8_t BRAKELIGHT_ANIMATION_SPEED_IDLE = 500;        // Скорость анимации выключения стоп-сигнала. Измеряеться в миллисекундах
+// Другое
+constexpr uint8_t BRAKELIGHT_STRIP_LEDCOUNT[2] = {8, 8, 8};     // Количество светодиодов в каждом отрезке. Пишеться в порядке сверху-вниз
 
 
 #include "microLED.h"                                           // Библиотека для адресной светодиодной ленты
@@ -29,14 +29,18 @@ constexpr uint8_t BRAKELIGHT_BRIGTNESS_IDLE = 20;
 unsigned long lightStripDelayLastCalled;                        // Переменная для замены delay() при помощи millis() в главной ленте
 unsigned long brakeLightOffDelayLastCalled;                     // Переменная для замены delay() при помощи millis() в стоп-сигнале
 
-LEDdata lightStripLEDs[LIGHT_STRIP_LED_COUNT];                              
-GRGB brakeLight(BRAKELIGHT_RED_PIN, BRAKELIGHT_GREEN_PIN, BRAKELIGHT_BLUE_PIN); // Обьект стоп-сигнала
+int brakeLightLEDCount = BRAKELIGHT_STRIP_LEDCOUNT[0] + BRAKELIGHT_STRIP_LEDCOUNT[1] + BRAKELIGHT_STRIP_LEDCOUNT[2]; // считать сумму всех светодиодов в ленте
+LEDdata lightStripLEDs[LIGHT_STRIP_LED_COUNT];       
+LEDdata brakeLightLEDs[brakeLightLEDCount];                       
+
+microLED brakeLight(brakeLightLEDs, brakeLightLEDCount, BRAKELIGHT_PIN);
 microLED mainLightStrip(lightStripLEDs, LIGHT_STRIP_LED_COUNT, LIGHT_STRIP_PIN); // Обьект главной светодиодной ленты
-MorsDuino arduinoLED(13);
+
+MorsDuino arduinoLED(LED_BUILTIN);
 
 void setup() {
   driverRunOnStartup();                                          // Стартовые функции драйвера
-  mainLightStrip.setBrightness(LIGHT_STRIP_BRIGHTNESS_ON);
+  mainLightStrip.setBrightness(LIGHT_STRIP_BRIGHTNESS);
   brakeLight.setBrightness(BRAKELIGHT_BRIGHTNESS_ON);
   // Настройка пинов
   pinMode(LIGHT_STRIP_PIN, OUTPUT);
@@ -49,7 +53,7 @@ void loop() {
   // Тут мы собираем данные с колеса
   
   eucCollectData();
-  int lightStripSpeed = eucLightStripSpeed();                   // Берем данные про скорость подсветки
+  int lightStripSpeed = eucLightStripSpeed();                     // Берем данные про скорость подсветки
   controlLights();
 }
 
