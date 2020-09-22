9f15e9f79fa6351774fbddfb03aecefcf80c3160
GGorAA <61655029+GGorAA@users.noreply.github.com>
2020-09-22 14:56:09 +0300

    Replaced constexpr with #define

--- euclight/euclight.ino
+++ euclight/euclight.ino
@@ -1,30 +1,29 @@
 // Настройки главной светодиодной ленты
-constexpr uint8_t LIGHT_STRIP_PIN = 6;				  // Пин, к которому подключена главная лента
-constexpr uint8_t LIGHT_STRIP_LED_COUNT = 144;		  // Количество светодиодов во всей главной ленте
-constexpr uint8_t LIGHT_STRIP_BRIGHTNESS = 50;		  // Яркость ленты. Диапазон: 0 - 255
-constexpr uint8_t LIGHT_STRIP_ANIMATION_MODIFIER = 2; // Модификатор для определения скорости ленты
-constexpr uint8_t LIGHT_STRIP_ANIMATION_STEP = 2;	  // Шаг радуги. Чем больше, тем меньше
-constexpr uint8_t LIGHT_STRIP_ANIMATION_MODE = 2;	  // Тип анимации. https://github.com/GGorAA/EUCLight/wiki/Animation-modes что бы узнать более
+#define LIGHT_STRIP_PIN = 6;                 // Пин, к которому подключена главная лента
+#define LIGHT_STRIP_LED_COUNT 144;         // Количество светодиодов во всей главной ленте
+#define LIGHT_STRIP_BRIGHTNESS 50;         // Яркость ленты. Диапазон: 0 - 255
+#define LIGHT_STRIP_ANIMATION_MODIFIER 2;  // Модификатор для определения скорости ленты
+#define LIGHT_STRIP_ANIMATION_STEP 2;      // Шаг радуги. Чем больше, тем меньше
+#define LIGHT_STRIP_ANIMATION_MODE 2;      // Тип анимации. https://github.com/GGorAA/EUCLight/wiki/Animation-modes что бы узнать более
 
 // Настройки стоп-сигнала
 // Пины
-constexpr uint8_t BRAKELIGHT_PIN = 5; // Пин подключения адресной ленты для стоп-сигнала
+#define BRAKELIGHT_PIN 5;  // Пин подключения адресной ленты для стоп-сигнала
 // Свет
-constexpr uint8_t BRAKELIGHT_BRIGHTNESS_ON = 255;  // Яркость включенного стоп-сигнала. Диапазон: 0 - 255
-constexpr uint8_t BRAKELIGHT_BRIGHTNESS_IDLE = 40; // Яркость бездейвствующего стоп-сигнала. Диапазон: 0 - 255
+#define BRAKELIGHT_BRIGHTNESS_ON 255;   // Яркость включенного стоп-сигнала. Диапазон: 0 - 255
+#define BRAKELIGHT_BRIGHTNESS_IDLE 40;  // Яркость бездейвствующего стоп-сигнала. Диапазон: 0 - 255
 // Другое
-constexpr uint8_t BRAKELIGHT_MATRIX_HEIGHT = 3;													   // Высота "матрицы" стоп-сигнала
-constexpr uint8_t BRAKELIGHT_MATRIX_WIDTH = 8;													   // Ширина "матрицы" стоп-сигнала
-constexpr uint8_t BRAKELIGHT_MATRIX_LEDCOUNT = BRAKELIGHT_MATRIX_WIDTH * BRAKELIGHT_MATRIX_HEIGHT; // Количество светодиодов в каждом отрезке. Пишеться в порядке сверху-вниз
-constexpr uint8_t BRAKELIGHT_SENSITIVITY = -5;													   // Чувствительность стоп-сигнала. Чем ближе к нулю, тем чувствительнее
+#define BRAKELIGHT_MATRIX_HEIGHT 3;                                                     // Высота "матрицы" стоп-сигнала
+#define BRAKELIGHT_MATRIX_WIDTH 8;                                                      // Ширина "матрицы" стоп-сигнала
+constexpr uint8_t BRAKELIGHT_MATRIX_LEDCOUNT = BRAKELIGHT_MATRIX_WIDTH * BRAKELIGHT_MATRIX_HEIGHT;  // Количество светодиодов в каждом отрезке. Пишеться в порядке сверху-вниз
+#define BRAKELIGHT_SENSITIVITY -5;                                                      // Чувствительность стоп-сигнала. Чем ближе к нулю, тем чувствительнее
 
-#define DEVICE_MODEL GotwayMcm2 // Модель моноколеса. Смотреть https://github.com/GGorAA/EUCLight для просмотра всех сущевствующих имен
+#define DEVICE_MODEL GotwayMcm2  // Модель моноколеса. Смотреть https://github.com/GGorAA/EUCLight для просмотра всех сущевствующих имен
 
 // ----------------------------НАСТРОЙКИ КОНЧИЛИСЬ------------------------ //
 
 #include "EUCSerialInterface.h"  // Библиотека для взаимодевствия с моноколесом
 #include "FastLED.h"             // Библиотека для адресных светодиодных лент
-#include "LightsControl.h"       // Файл для функций ленты
 #include "MorsDuino.h"           // Библиотека для кода морзе
 
 unsigned long lightStripDelayLastCalled;     // Переменная для замены delay() при помощи millis() в главной ленте
@@ -55,9 +54,9 @@ void setup() {
   pinMode(BRAKELIGHT_PIN, OUTPUT);
 
   Serial.begin(115200);
-  ElectricUnicycle.setCallback(eucCallbackFunction);
-  FastLED.addLeds<WS2811, LIGHT_STRIP_PIN>(mainLightStrip, LIGHT_STRIP_LED_COUNT); // Добавить главную ленту
-  FastLED.addLeds<WS2811, BRAKELIGHT_PIN>(brakeLight, BRAKELIGHT_MATRIX_LEDCOUNT); // Добавить стоп-сигнал
+  ElectricUnicycle.setCallback(eucCallbackFunction);                                // Функция которая будет запущена библиотекой EUCSerialInterface при каждом вызове ElectricUnicycle.tick()
+  FastLED.addLeds<WS2811, LIGHT_STRIP_PIN>(mainLightStrip, LIGHT_STRIP_LED_COUNT);  // Добавить главную ленту
+  FastLED.addLeds<WS2811, BRAKELIGHT_PIN>(brakeLight, BRAKELIGHT_MATRIX_LEDCOUNT);  // Добавить стоп-сигнал
 }
 
 void loop() {
